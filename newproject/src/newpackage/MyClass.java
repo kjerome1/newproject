711f4f8f933a6d4dd2b60f01d86da16b262cc849
Kevin Jerome <kevin.jerome@platotesting.com>
2019-02-26 16:01:13 -0400

    Updated MyClass.java

--- newproject/src/newpackage/MyClass.java
+++ newproject/src/newpackage/MyClass.java
@@ -1,5 +1,37 @@
 package newpackage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MyClass {
 
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\ChromeDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		//driver.get("http://www.google.com");
+		
+		String baseUrl = "http://demo.guru99.com/test/newtours/";
+		String expectedTitle = "Welcome: Mercury Tours";
+		String actualTitle ="";
+		
+		//launch Chrome and direct it to the Base URL
+		driver.get(baseUrl);
+		
+		//get the actual value of the title
+		actualTitle = driver.getTitle();
+		
+		/*
+		 * Compare the actual title of the page with the expected one
+		 * and print the result as "Passed" or "Failed"
+		 */
+		if(actualTitle.contentEquals(expectedTitle)){
+			System.out.println("Test Passed!");	
+		} else{
+			System.out.println("Test Failed");
+		}		
+
+		//close Chrome
+		driver.close();
+	}
 }
