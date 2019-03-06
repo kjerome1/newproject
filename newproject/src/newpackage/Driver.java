e16aebef9def3509e33d2a8f18ac6f554d16ce77
Kevin Jerome <kevin.jerome@platotesting.com>
2019-03-06 11:51:20 -0400

    Updated Driver.java

--- newproject/src/newpackage/Driver.java
+++ newproject/src/newpackage/Driver.java
@@ -49,7 +49,7 @@ public class Driver {
 	}
 	
 	/**
-	 *  Enter username in the login form
+	 * Enter username in the login form
 	 * @param driver - WebDriver
 	 * @param user - String
 	 */
@@ -110,14 +110,21 @@ public class Driver {
 		}
 	}
 	
-	public static void selectFlightType(WebDriver driver){
+	/**
+	 * Select a flight type radio button
+	 * @param driver - WebDriver
+	 * @param type String, options are roundtrip, oneway.
+	 */
+	public static void selectFlightType(WebDriver driver, String type){
 		try{
-			driver.findElement(By.xpath("//input[@value='roundtrip']")).click();
+			driver.findElement(By.xpath("//input[@type='radio' and @value='"+type+"']")).click();
 			//driver.findElement(By.xpath("//input[@value='oneway']")).click();
 		} catch(Exception e){
+			System.out.print("Cannot find '"+type+"' as an option.");
 			System.err.println(e);
 		}
 	}
+	
 	/**
 	 * Select the number of passengers on the Flight Finder page, integer value between 1-4
 	 * @param driver - WebDriver
@@ -130,19 +137,6 @@ public class Driver {
 			} else {
 				System.out.println(n+" is out of bounds! Value must be 1-4");
 			}			
-		} catch(Exception e){
-			System.err.println(e);
-		}
-	}
-	
-	/**
-	 * Select a city for the 'Arriving In' drop down menu on the Flight Finder page
-	 * @param driver - WebDriver
-	 * @param city - String
-	 */
-	public static void selectArrivingIn(WebDriver driver, String city){
-		try{
-			driver.findElement(By.xpath("//select[@name='toPort']/option[@value='"+city+"']")).click();
 		} catch(Exception e){
 			System.err.println(e);
 		}
@@ -162,17 +156,71 @@ public class Driver {
 	}
 	
 	/**
-	 * Select a 'Service Class' radio button on the Flight Finder page
+	 * Select a departure date (month/day)
 	 * @param driver - WebDriver
+	 * @param month - integer, 1=January, 2=February, etc. 
+	 * @param day - integer
 	 */
-	public static void selectServiceClass(WebDriver driver){
+	public static void departingDate(WebDriver driver, int month, int day){
 		try{
-			driver.findElement(By.xpath("//input[@value = 'Coach']")).click(); //options: Coach, Business, First
+			driver.findElement(By.xpath("//select[@name='fromMonth']/option[@value='"+month+"']")).click(); 
+			driver.findElement(By.xpath("//select[@name='fromDay']/option[@value='"+day+"']")).click();
 		} catch (Exception e){
 			System.err.println(e);
 		}
 	}
 	
+	/**
+	 * Select a city for the 'Arriving In' drop down menu on the Flight Finder page
+	 * @param driver - WebDriver
+	 * @param city - String
+	 */
+	public static void selectArrivingIn(WebDriver driver, String city){
+		try{
+			driver.findElement(By.xpath("//select[@name='toPort']/option[@value='"+city+"']")).click();
+		} catch(Exception e){
+			System.err.println(e);
+		}
+	}
 	
+	/**
+	 * Select an arrival date (month/day) for the 'Returning' month/day drop down menus
+	 * @param driver - WebDriver
+	 * @param month - integer, 1=January, 2=February, etc. 
+	 * @param day - integer
+	 */
+	public static void arivalDate(WebDriver driver, int month, int day){
+		try{
+			driver.findElement(By.xpath("//select[@name='toMonth']/option[@value='"+month+"']")).click(); 
+			driver.findElement(By.xpath("//select[@name='toDay']/option[@value='"+day+"']")).click();
+		} catch (Exception e){
+			System.err.println(e);
+		}
+	}
 	
+	/**
+	 * Select a 'Service Class' radio button on the Flight Finder page
+	 * @param driver - WebDriver
+	 * @param serviceClass - String, options: Coach, Business, First
+	 */
+	public static void selectServiceClass(WebDriver driver, String serviceClass){
+		try{
+			driver.findElement(By.xpath("//input[@value = '"+serviceClass+"']")).click(); //options: Coach, Business, First
+		} catch (Exception e){
+			System.err.println(e);
+		}
+	}
+	
+	/**
+	 * Select a Airline under Preferences on the Flight Finder page.
+	 * @param driver - WebDriver
+	 * @param airline - String, options are: No Preference, Blue Skies Airlines, Unified Airlines, Pangea Airlines.
+	 */
+	public static void selectAirline(WebDriver driver, String airline){
+		try{
+			driver.findElement(By.xpath("//select[@name='airline']/option[@value='"+airline+"']")).click();
+		} catch(Exception e){
+			System.err.println(e);
+		}
+	}	
 }
